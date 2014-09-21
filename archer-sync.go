c97e46b069a716157c0f012d53d5f43fd88d42da
akihito <takeda.akihito@gmail.com>
2014-09-22 00:59:23 +0900

    channel buffer (parallel)

--- archer-sync.go
+++ archer-sync.go
@@ -17,25 +17,35 @@ func main() {
 
     for project, servers := range projects {
         msg := make(chan string)
-        log.Printf("\nmsg:%v\n", servers["servers"])
-        log.Printf("\nmsg:%v\n", project)
-        for i := range servers["servers"] {
-            go func(n int) {
-//            msg := Rsync(rsyncOption)
-                log.Printf("\n 0xxxxxxxxxx nserver:%s, msg:%i, \n", msg, n)
-//if ( n == 3 ) {
-//    n = 7
-//}
-time.Sleep( time.Duration(n) * time.Second )
-                log.Printf("\n 1xxxxxxxxxx nserver:%s, msg:%s, \n", msg, n)
-                res := "xyz"
-                msg <- res
-            }(i)
-            log.Printf("\n+++++ 1 server:%s\n", servers["servers"][i])
-        }
+        parallel := make(chan bool, 4)
+        log.Printf("\n\n\n\n msg:%v\n", servers["servers"])
+        log.Printf("\nproject:%v\n", project)
+go func() {
+        for number, target  := range servers["servers"] {
+            select {
+            case para <- true:
 
+log.Printf("\n >> serv:%s, i:%i, \n", target, number)
+                go func(n int, server string) {
+                    log.Printf("\n start serv:%s, msg:%s, n:%i, \n", server, msg, n)
+    //            msg := Rsync(rsyncOption)
+//    if ( n == 3 ) {
+//        n = 7
+//    }
+    time.Sleep( time.Duration(n) * time.Second )
+//    time.Sleep( time.Duration(1) * time.Second )
+//                    log.Printf("\n end server:%s, msg:%s, n:%i, \n", servers["servers"][i], msg, n)
+                    res := fmt.Sprintf("xyz:%s", server)
+                    msg <- res
+                    <- para
+                }(number, target)
+            }
+
+            log.Printf("\nserver:%s\n", servers["servers"][number])
+        }
+}()
     for j := 0; j < len(servers["servers"]); j++ { 
-        log.Printf("\n+++++ 2 msg:%s\n", <-msg)
+        log.Printf("\nresult >>> msg:%s\n", <-msg)
     }
 
 
